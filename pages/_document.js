d9e1af94fc673f13b760bb81e31ee203c5db1703
Filip Jakov Bulic <filip.jakov.bulic@infinum.hr>
2021-02-21 21:50:34 +0100

    feat: favicon support and additional meta

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ import Document, {
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" dir="rtl">
         <Head>
           <link
             rel="preload"
@@ -16,6 +16,47 @@ class MyDocument extends Document {
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon-16x16.png"
+          />
+          <link rel="manifest" href="site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta
+            name="msapplication-TileColor"
+            content="#da532c"
+          />
+          {/* TODO: not working */}
+          <meta name="theme-color" content="#f96a58" />
+          {/* To run web application in full-screen*/}
+          <meta
+            name="apple-mobile-web-app-capable"
+            content="yes"
+          />
+
+          {/* Status Bar Style (see Supported Meta Tags below for available values) */}
+          {/* Has no effect unless you have the previous meta tag*/}
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black"
           />
         </Head>
         <body>
